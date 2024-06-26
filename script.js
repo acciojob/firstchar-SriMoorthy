e1ddce579f83a18a4db3debbd1b3c1834669b816
SriMoorthy <srimoorthy27@gmail.com>
2024-06-26 11:06:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+	let T = text.trim()
+	if(T == ' ' && T == ''){
+		return ('')
+	}
+	else{
+		return T[0]
+	}
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
